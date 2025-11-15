8d4477f9f0c36f27ef163a1ffb9cc79c12adab5c
jack-mcnulty <jack.mcnulty@stu.ocsb.ca>
2025-11-15 12:27:06 -0500

    Turns the microbit into a working radio

--- main.ts
+++ main.ts
@@ -1,8 +1,42 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jack
+ * Created on: Nov 2025
+ * This program turns the microbit into a working radio
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+
+// setup
+radio.setGroup(12)
+basic.showIcon(IconNames.Happy)
+
+// recieving
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
+
+// getting distance
+while (true) {
+    distanceToObject = sonar.ping(
+        DigitalPin.P12,
+        DigitalPin.P13,
+        PingUnit.Centimeters
+    )
+
+    // if distance is less than 5
+    if (distanceToObject < 5) {
+        basic.clearScreen()
+        basic.showNumber(distanceToObject)
+        radio.sendString("Too close")
+        basic.pause(1000)
+    } else {
+        basic.clearScreen()
+        basic.showNumber(distanceToObject)
+        radio.sendString("")
+        basic.pause(1000)
+    }
+}
