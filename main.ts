3fdcd6df3d104db575db9259562a2853530152d7
jack-mcnulty <jack.mcnulty@stu.ocsb.ca>
2025-12-10 13:25:11 -0500

    added code

--- main.ts
+++ main.ts
@@ -18,25 +18,3 @@ radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
     basic.showIcon(IconNames.Happy)
 })
-
-// getting distance
-while (true) {
-    distanceToObject = sonar.ping(
-        DigitalPin.P12,
-        DigitalPin.P13,
-        PingUnit.Centimeters
-    )
-
-    // if distance is less than 5
-    if (distanceToObject < 5) {
-        basic.clearScreen()
-        basic.showNumber(distanceToObject)
-        radio.sendString("Too close")
-        basic.pause(1000)
-    } else {
-        basic.clearScreen()
-        basic.showNumber(distanceToObject)
-        radio.sendString("")
-        basic.pause(1000)
-    }
-}
